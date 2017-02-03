3e2b95fe2d7618296161a347765d91b80a2c938a
root <root@dabelson.cse356.compas.cs.stonybrook.edu>
2017-02-03 17:59:47 -0500

    Working on HTML and CSS

--- eliza/__init__.py
+++ eliza/__init__.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+import sys
+
 app = Flask(__name__)
+#root = os.path.dirname(__file__)
+#template_path = os.path.join(root, 'eliza/templates')
+#app = Flask(__name__, template_folder= template_path)
 @app.route("/")
 def hello():
-	return "FUCKING FINALLY"
+	return "FINALLY"
 
 @app.route('/eliza/')
 def eliza():
-	return "ELIZA"
+	return render_template('eliza.html')
 
 if __name__=="__main__":
-	app.run(host='0.0.0.0', port=80)
-
+	#app.run(host='0.0.0.0', port=80)
+	app.run(debug = True)
