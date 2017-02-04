908a2de1fc27b6eb9d2af23f66385d1a09693f16
root <root@dabelson.cse356.compas.cs.stonybrook.edu>
2017-02-03 19:33:47 -0500

    finished parts 1 and 2

--- eliza/__init__.py
+++ eliza/__init__.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 import sys
+import time
 
 app = Flask(__name__)
-#root = os.path.dirname(__file__)
-#template_path = os.path.join(root, 'eliza/templates')
-#app = Flask(__name__, template_folder= template_path)
 @app.route("/")
 def hello():
 	return "FINALLY"
 
-@app.route('/eliza/')
+@app.route('/eliza/', methods=['GET', 'POST'])
 def eliza():
-	return render_template('eliza.html')
+	if request.method == "POST":
+		name = request.form['name']
+		date = time.strftime("%c")
+		return render_template('elizapost.html', name=name, date=date)
+	else:
+		return render_template('eliza.html')
 
 if __name__=="__main__":
 	#app.run(host='0.0.0.0', port=80)
